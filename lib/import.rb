44b65f3fd2841b17c93cea8b1d6578d1752e7cce
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-01 09:17:22 +0900

    Refactor

--- lib/import.rb
+++ lib/import.rb
@@ -11,13 +11,12 @@ module Import
       return evaluate(path)
     end
 
-    define_method(:global) do
-      module ::Kernel
-        def import(feature)
-          Import.import(feature, caller_locations[0].absolute_path)
-        end
-      end
+    def global
+      ::Kernel.__send__(:define_method, :import, -> (feature) {
+        Import.import(feature, caller_locations[0].absolute_path)
+      })
     end
+
 
     private
 
@@ -96,7 +95,7 @@ module Import
         if full_path
           this.__send__(:evaluate, full_path, mod)
         else
-          require feature
+          require feature #TODO: Does not work...
         end
       }
     end
